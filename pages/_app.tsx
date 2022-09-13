277e38f43ef5e8429bfeeff8389d5d577970646b
tmh9x <thomas.hermanu@gmail.com>
2022-09-13 15:55:07 +0200

    add mantineprovider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import type { AppProps } from "next/app";
+import { MantineProvider } from "@mantine/core";
+
+function MyApp({}: AppProps) {
+  return (
+    <>
+      <MantineProvider
+        theme={{
+          // Override any other properties from default theme
+          fontFamily: "Open Sans, sans serif",
+          spacing: { xs: 15, sm: 20, md: 25, lg: 30, xl: 40 },
+        }}
+      >
+        <div></div>
+      </MantineProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
