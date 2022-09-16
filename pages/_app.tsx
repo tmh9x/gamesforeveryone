443e7fcaf7ec3b86592dbc75dcc3e1549ae91682
tmh9x <thomas.hermanu@gmail.com>
2022-09-16 16:22:07 +0200

    switch back to mui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,13 @@
 import "../styles/globals.css";
 
 import type { AppProps } from "next/app";
-import { MantineProvider } from "@mantine/core";
+import NavBar from "../components/NavBar/NavBar";
 
-function MyApp({}: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <MantineProvider
-        theme={{
-          // Override any other properties from default theme
-          fontFamily: "Open Sans, sans serif",
-          spacing: { xs: 15, sm: 20, md: 25, lg: 30, xl: 40 },
-        }}
-      >
-        <div></div>
-      </MantineProvider>
+      <NavBar />
+      <Component {...pageProps} />
     </>
   );
 }
