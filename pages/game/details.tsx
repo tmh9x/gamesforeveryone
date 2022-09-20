8226409a833caf2ca31463827b15424b2fa43bb6
tmh9x <thomas.hermanu@gmail.com>
2022-09-20 10:49:28 +0200

    add details page and change styles

---
+++ pages/game/details.tsx
@@ -0,0 +1,12 @@
+import GameDetails from "../../components/GameDetails/GameDetails";
+import type { NextPage } from "next";
+
+const Details: NextPage = () => {
+  return (
+    <div>
+      <GameDetails />
+    </div>
+  );
+};
+
+export default Details;
