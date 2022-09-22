115d1c070eb5242fa19bdf6ad9cedd54f278a1e4
tmh9x <thomas.hermanu@gmail.com>
2022-09-22 12:06:37 +0200

    ✨ get gamedata from firestore db

--- components/GameCard/GameCard.tsx
+++ components/GameCard/GameCard.tsx
@@ -1,34 +1,13 @@
-import IconButton, { IconButtonProps } from "@mui/material/IconButton";
-import React, { useEffect } from "react";
-import { collection, getDocs } from "firebase/firestore";
-
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import CardMedia from "@mui/material/CardMedia";
 import FavoriteIcon from "@mui/icons-material/Favorite";
+import IconButton from "@mui/material/IconButton";
 import Link from "next/link";
 import { Typography } from "@mui/material";
-import { db } from "../../firebase/config";
-import styles from "./Card.module.css";
+import styles from "./GameCard.module.css";
 
-export default function GameCard() {
-  console.log("db", db);
-
-  const getGames = async () => {
-    try {
-      const querySnapshot = await getDocs(collection(db, "games"));
-      querySnapshot.forEach((doc) => {
-        console.log(`${doc.id} => ${doc.data()}`);
-      });
-    } catch (error) {
-      console.log("error", error);
-    }
-  };
-
-  useEffect(() => {
-    getGames();
-  }, []);
-
+export default function GameCard(game: Game) {
   return (
     <Card sx={{ maxWidth: 345 }} className={styles.card}>
       <CardHeader title="PLATFORM" className={styles.card_header} />
@@ -42,8 +21,8 @@ export default function GameCard() {
       </Link>
       <div className={styles.card_content}>
         <div>
-          <Typography variant="h5">GAMENAME</Typography>
-          <Typography paragraph>PRICE</Typography>
+          <Typography variant="h5">{game.title}</Typography>
+          <Typography paragraph>${game.price}</Typography>
         </div>
         <div>
           <IconButton aria-label="add to favorites">
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,60 @@
+import { collection, getDocs } from "firebase/firestore";
+import { useEffect, useState } from "react";
+
 import Carousel from "../components/Carousel/Carousel";
-import GameCard from "../components/Card/Card";
+import GameCard from "../components/GameCard/GameCard";
 import type { NextPage } from "next";
+import { db } from "../firebase/config";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
+  console.log("db", db);
+  const [games, setGames] = useState<Games>([]);
+
+  const getGames = async () => {
+    let dataArray: Games = [];
+
+    try {
+      const querySnapshot = await getDocs(collection(db, "games"));
+      querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+        console.log("DATA", doc.data());
+        const gamesData = doc.data() as Game;
+        dataArray.push(gamesData);
+        setGames(dataArray);
+      });
+    } catch (error) {
+      console.log("error getgames", error);
+    }
+    console.log("dataArray", dataArray);
+  };
+
+  useEffect(() => {
+    getGames();
+  }, []);
+
+  console.log("games", games);
+
   return (
     <div className={styles.container}>
       <Carousel />
-      <GameCard />
+      {games &&
+        games.map((game: Game, i: number) => (
+          <div key={i}>
+            <GameCard
+              title={game.title}
+              price={game.price}
+              creator={game.creator}
+              description={game.description}
+              fsk={game.fsk}
+              platform={game.platform}
+              year={game.year}
+              image={game.image}
+              genre={game.genre}
+              amount={game.amount}
+            />
+          </div>
+        ))}
     </div>
   );
 };
