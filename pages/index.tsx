02c5fb3e588636f5094142d91df500a281a81b50
halilesmer <halil@esmer.de>
2022-09-26 16:20:44 +0200

    🎨 add dynamic route

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ const Home: NextPage = () => {
         console.log(`${doc.id} => ${doc.data()}`);
         console.log("DATA", doc.data());
         const gamesData = doc.data() as Game;
-        dataArray.push(gamesData);
+        dataArray.push({...gamesData, gameId: doc.id});
         setGames(dataArray);
       });
     } catch (error) {
@@ -52,6 +52,7 @@ const Home: NextPage = () => {
               image={game.image}
               genre={game.genre}
               amount={game.amount}
+              gameId={game.gameId}
             />
           </div>
         ))}
